b33e023bcca7476da466727febdc5115987b1906
Itishiei <135040928+Itishiei@users.noreply.github.com>
2023-07-24 19:17:04 +0000

    Update styles.js

--- src/screens/Categories/styles.js
+++ src/screens/Categories/styles.js
@@ -1,6 +1,15 @@
 import { StyleSheet } from 'react-native';
 
-
+const styles = StyleSheet.create({
+  categoriesItemContainer: {
+    flex: 1,
+    margin: 10,
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 215,
+    borderColor: '#cccccc',
+    borderWidth: 0.5,
+    borderRadius: 20,
   },
   categoriesPhoto: {
     width: '100%',
