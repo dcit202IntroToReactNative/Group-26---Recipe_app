07b5aed436b02e98c916e476ee72b81971233e78
Itishiei <135040928+Itishiei@users.noreply.github.com>
2023-07-24 19:15:03 +0000

    Update CategoriesScreen.js

--- src/screens/Categories/CategoriesScreen.js
+++ src/screens/Categories/CategoriesScreen.js
@@ -1,4 +1,4 @@
-import React, { useLayoutEffect } from "react";
+import , { useLayoutEffect } from "react";
 import { FlatList, Text, View, Image, TouchableHighlight } from "react-native";
 import styles from "./styles";
 import { categories } from "../../data/dataArrays";
